9c569d32d075d98cf9fc9114ca1267bb26084d29
jcwall <johncodywall@gmail.com>
2017-03-29 13:29:29 -0600

    first script

---
+++ website_blocker.py
@@ -0,0 +1,30 @@
+import time
+from datetime import datetime as dt
+
+#hosts_temp=r"D:\Dropbox\pp\block_websites\Demo\hosts"
+hosts_path="/etc/hosts"
+ip="127.0.0.1"
+website_list=["facebook.com", "espn.com", "sports.yahoo.com", "wired.com", "reddit.com", "fanduel.com"]
+www_list = ["www."+i for i in website_list]
+www_list.extend(website_list)
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,6) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,20):
+        print("Get to work!!")
+        with open(hosts_path,'r+') as file:
+            content=file.read()
+            for website in www_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(ip+" "+ website+"\n")
+    else:
+        with open(hosts_path,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in www_list):
+                    file.write(line)
+            file.truncate()
+        print("Feed the sports addiction!!")
+    time.sleep(3600)
